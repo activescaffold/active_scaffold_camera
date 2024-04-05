79955994e7f0f961475902ae9c904a7adc35a656
Sergio Cambra <sergio@enpijama.es>
2024-04-05 17:53:31 +0200

    use hooks to add helpers to ActionView::Base

--- lib/active_scaffold_camera.rb
+++ lib/active_scaffold_camera.rb
@@ -7,6 +7,5 @@ module ActiveScaffoldCamera
   autoload 'ViewHelpers', 'active_scaffold_camera/view_helpers.rb'
 end
 
-ActionView::Base.send :include, ActiveScaffoldCamera::ViewHelpers
 ActiveScaffold.stylesheets << 'active_scaffold_camera'
 ActiveScaffold.javascripts << 'active_scaffold_camera'
--- lib/active_scaffold_camera/engine.rb
+++ lib/active_scaffold_camera/engine.rb
@@ -1,4 +1,9 @@
 module ActiveScaffoldCamera
   class Engine < ::Rails::Engine
+    initializer 'active_scaffold_camera.action_view' do
+      ActiveSupport.on_load :action_view do
+        include ActiveScaffoldCamera::ViewHelpers
+      end
+    end
   end
 end
